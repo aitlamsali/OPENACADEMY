5d02af1aa094020a59b75ff3979cc441942ab803
Aitlamsali <italaa86@gmail.com>
2017-09-11 00:42:03 +0100

    oo

--- models/models.py
+++ models/models.py
@@ -16,11 +16,53 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                     domain=['|', ('instructor', '=', True),('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats=0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+                # r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+
+    @api.onchange('seats', 'attendee_ids')
+    def _check_valid_seats(self):
+        if self.seats < 0:
+            return {
+                    'warning':{
+                        'title':"incorrect 'seat' value",
+                        'message':"the number of seats cannot be negative",
+                                },
+                    }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendee maximum exceeded",
+                    'message': "incrementer le nombre de seat ou diminuer le nombre candidat",
+                },
+            }
+
+
+
+
+
+
+    # @api.depends('seats', 'attendee_ids')
+    # def _taken_seats(self):
+    #     for r in self:
+    #         if not r.seats:
+    #             r.taken_seats = 0.0
+    #         else:
+    #             r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
